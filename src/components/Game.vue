304a74f9fdff7cb65373022b1f17aef1b7408cfe
David Witt <midknight55@gmail.com>
2017-11-19 09:56:43 -0600

    Added API call to movie database
    Added Axios to make GET request
    Refined search to a reasoanbly guessable set of movies
    TODO hook up min/max year

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -8,12 +8,38 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'Game',
   data () {
     return {
 
     }
+  },
+  beforeRouteEnter (to, from, next) {
+    /**
+     * Get popular movies between year range
+     * Each page returns 20 results, get a random page 1-10
+     * Only include English movies, exclude adult genre
+     */
+    axios.get('https://api.themoviedb.org/3/discover/movie', {
+      params: {
+        api_key: 'c4ad04648c8511d24d60ef4965cb2e52',
+        page: Math.floor(Math.random() * 10) + 1,
+        sort_by: 'popularity.desc',
+        with_original_language: 'en',
+        include_adult: false,
+        'primary_release_date.gte': '1990',
+        'primary_release_date.lte': '2010'
+      }
+    })
+    .then(resp => {
+      next(vm => Object.assign(vm.$data, {movieData: resp.data}))
+    })
+    .catch(err => {
+      console.log(err)
+    })
   }
 }
 </script>

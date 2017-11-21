f25a4fc1837bdbd231bd23e2d41edf1f342920e9
David Witt <midknight55@gmail.com>
2017-11-20 21:31:08 -0600

    Added poster and backdrop to movie
    Roughed out some styles and layout.
    Will finish the cleanup in following commit.
    Everything should remain functional, but ugly

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -1,30 +1,47 @@
 <template>
-  <div>
-
-    {{this.info.title}}
-    <div>
-      {{this.info.tagline}}
+  <div class="movie-wrapper">
+    <div class="movie-backdrop">
+      <img :src="backdropImg" :alt="this.info.title"/>
     </div>
 
-    <div v-if="showGenre">
-      {{genre}}
+    <div class="movie-overlay">
+      <div class="movie-overlay-content">
+
+        <div class="movie-overlay-upper">
+          <figure class="movie-poster">
+            <img :src="posterImg" :alt="this.info.title"/>
+          </figure>
+
+          <div class="movie-info">
+            <div v-if="showTitle">
+              {{this.info.title}}
+            </div>
+            <div>
+              {{this.info.tagline}}
+            </div>
+
+            <div v-if="showGenre">
+              {{genre}}
+            </div>
+
+            <div v-if="showRelease">
+              {{this.info.release_date}}
+            </div>
+
+            <div v-if="showStarring">
+              {{starring}}
+            </div>
+          </div>
+        </div>
+
+        <div class="movie-summary">
+          <div v-if="showOverview">
+            {{this.info.overview}}
+          </div>
+        </div>
+      </div>
     </div>
 
-    <div v-if="showRelease">
-      {{this.info.release_date}}
-    </div>
-
-    <div v-if="showStarring">
-      {{starring}}
-    </div>
-
-    <div v-if="showOverview">
-      {{this.info.overview}}
-    </div>
-
-    <div v-if="showTitle">
-      {{this.info.title}}
-    </div>
   </div>
 </template>
 
@@ -77,6 +94,12 @@ export default {
       let genreList = []
       take(this.info.genres, 3).map((genre) => { genreList.push(genre.name) })
       return genreList.join(', ')
+    },
+    backdropImg: function () {
+      return `https://image.tmdb.org/t/p/w780/${this.info.backdrop_path}`
+    },
+    posterImg: function () {
+      return `https://image.tmdb.org/t/p/w342/${this.info.poster_path}`
     }
   },
   methods: {
@@ -93,5 +116,55 @@ export default {
 </script>
 
 <style scoped>
+  .movie-wrapper {
+    position: relative;
+  }
 
+  .movie-backdrop img {
+    width: 100%;
+    filter: opacity(100) grayscale(100%) contrast(130%);
+    z-index: 1;
+  }
+
+  .movie-backdrop:before {
+    content: '';
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background: linear-gradient(45deg,#a177ff,#02ceff);
+    opacity: .7;
+    z-index: 2;
+  }
+
+  .movie-overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 3;
+  }
+
+  .movie-overlay-upper {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: flex-start;
+    flex-wrap: wrap;
+    color: #fff;
+  }
+
+  .movie-poster {
+    flex: 1;
+  }
+
+  .movie-info {
+    flex: 2;
+  }
+
+  .movie-summary {
+    color: #fff;
+  }
 </style>
